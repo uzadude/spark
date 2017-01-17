cfbc5cc0d19b02059340c0ef75cc32c0e78cd70f
hyukjinkwon <gurwls223@gmail.com>
2017-01-17 12:28:15 +0000

    [SPARK-3249][DOC] Fix links in ScalaDoc that cause warning messages in `sbt/sbt unidoc`
    ## What changes were proposed in this pull request?
    
    This PR proposes to fix ambiguous link warnings by simply making them as code blocks for both javadoc and scaladoc.
    
    ```
    [warn] .../spark/core/src/main/scala/org/apache/spark/Accumulator.scala:20: The link target "SparkContext#accumulator" is ambiguous. Several members fit the target:
    [warn] .../spark/mllib/src/main/scala/org/apache/spark/mllib/optimization/GradientDescent.scala:281: The link target "runMiniBatchSGD" is ambiguous. Several members fit the target:
    [warn] .../spark/mllib/src/main/scala/org/apache/spark/mllib/fpm/AssociationRules.scala:83: The link target "run" is ambiguous. Several members fit the target:
    ...
    ```
    
    This PR also fixes javadoc8 break as below:
    
    ```
    [error] .../spark/sql/core/target/java/org/apache/spark/sql/LowPrioritySQLImplicits.java:7: error: reference not found
    [error]  * newProductEncoder - to disambiguate for {link List}s which are both {link Seq} and {link Product}
    [error]                                                   ^
    [error] .../spark/sql/core/target/java/org/apache/spark/sql/LowPrioritySQLImplicits.java:7: error: reference not found
    [error]  * newProductEncoder - to disambiguate for {link List}s which are both {link Seq} and {link Product}
    [error]                                                                                ^
    [error] .../spark/sql/core/target/java/org/apache/spark/sql/LowPrioritySQLImplicits.java:7: error: reference not found
    [error]  * newProductEncoder - to disambiguate for {link List}s which are both {link Seq} and {link Product}
    [error]                                                                                                ^
    [info] 3 errors
    ```
    
    ## How was this patch tested?
    
    Manually via `sbt unidoc > output.txt` and the checked it via `cat output.txt | grep ambiguous`
    
    and `sbt unidoc | grep error`.
    
    Author: hyukjinkwon <gurwls223@gmail.com>
    
    Closes #16604 from HyukjinKwon/SPARK-3249.

--- graphx/src/main/scala/org/apache/spark/graphx/GraphOps.scala
+++ graphx/src/main/scala/org/apache/spark/graphx/GraphOps.scala
@@ -439,7 +439,7 @@ class GraphOps[VD: ClassTag, ED: ClassTag](graph: Graph[VD, ED]) extends Seriali
    * Compute the connected component membership of each vertex and return a graph with the vertex
    * value containing the lowest vertex id in the connected component containing that vertex.
    *
-   * @see [[org.apache.spark.graphx.lib.ConnectedComponents$#run]]
+   * @see `org.apache.spark.graphx.lib.ConnectedComponents.run`
    */
   def connectedComponents(): Graph[VertexId, ED] = {
     ConnectedComponents.run(graph)
@@ -449,7 +449,7 @@ class GraphOps[VD: ClassTag, ED: ClassTag](graph: Graph[VD, ED]) extends Seriali
    * Compute the connected component membership of each vertex and return a graph with the vertex
    * value containing the lowest vertex id in the connected component containing that vertex.
    *
-   * @see [[org.apache.spark.graphx.lib.ConnectedComponents$#run]]
+   * @see `org.apache.spark.graphx.lib.ConnectedComponents.run`
    */
   def connectedComponents(maxIterations: Int): Graph[VertexId, ED] = {
     ConnectedComponents.run(graph, maxIterations)
